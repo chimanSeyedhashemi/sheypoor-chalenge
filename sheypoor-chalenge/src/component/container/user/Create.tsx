80eeaa8a3cae04e9da2e3255d494c72080f270b1
ch.hashemi <ch.seyedhashemi@gmail.com>
2021-06-05 16:08:09 +0430

    add form one to create cmp

--- sheypoor-chalenge/src/component/container/user/Create.tsx
+++ sheypoor-chalenge/src/component/container/user/Create.tsx
@@ -1,3 +1,10 @@
+import FormBox from "../../share/FormBox";
+import { FormOne } from "./FormOne";
+
 export const CreatUser = () => {
-  return <>create user</>;
+  return (
+    <FormBox title="create user">
+      <FormOne />
+    </FormBox>
+  );
 };
