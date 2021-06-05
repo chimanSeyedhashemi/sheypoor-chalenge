606c48391e09053dc3587a1acb3474dfaf04ade5
ch.hashemi <ch.seyedhashemi@gmail.com>
2021-06-05 10:24:58 +0430

    restructre files managment

---
+++ sheypoor-chalenge/src/component/container/app/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { CreatUser } from "../user/Create";
+
+function App() {
+  return (
+    <div className="App">
+      <CreatUser />
+    </div>
+  );
+}
+
+export default App;
